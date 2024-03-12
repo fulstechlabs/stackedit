e6336c741a7cd6cb424bcd0ca83e3bb274247343
Hai Nguyen <nguyenminhhai2209@gmail.com>
2024-03-13 01:13:19 +0700

    Fix navigation bar button alignment

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -6,10 +6,10 @@
       <button class="navigation-bar__button navigation-bar__button--explorer-toggler button" v-else tour-step-anchor="explorer" @click="toggleExplorer()" v-title="'Toggle explorer'"><icon-folder></icon-folder></button>
     </div> -->
     <!-- Side bar -->
-    <div class="navigation-bar__inner navigation-bar__inner--right navigation-bar__inner--button">
+    <!-- <div class="navigation-bar__inner navigation-bar__inner--right navigation-bar__inner--button">
       <a class="navigation-bar__button navigation-bar__button--stackedit button" v-if="light" href="app" target="_blank" v-title="'Open StackEdit'"><icon-provider provider-id="stackedit"></icon-provider></a>
-      <!-- <button class="navigation-bar__button navigation-bar__button--stackedit button" v-else tour-step-anchor="menu" @click="toggleSideBar()" v-title="'Toggle side bar'"><icon-provider provider-id="stackedit"></icon-provider></button> -->
-    </div>
+      <button class="navigation-bar__button navigation-bar__button--stackedit button" v-else tour-step-anchor="menu" @click="toggleSideBar()" v-title="'Toggle side bar'"><icon-provider provider-id="stackedit"></icon-provider></button>
+    </div> -->
     <div class="navigation-bar__inner navigation-bar__inner--right navigation-bar__inner--title flex flex--row">
       <!-- Spinner -->
       <div class="navigation-bar__spinner">
